b176d024854afd1a21cb8bea752c7bf1a1806828
AdrianGerman <adrian.german1019@gmail.com>
2026-03-07 19:48:02 -0600

    apply components in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import "./App.css"
+import ChannelSection from "./components/ChannelSection"
+import Hero from "./components/Hero"
+import Navbar from "./components/Navbar"
+import StatsBar from "./components/StatsBar"
+import { CHANNELS } from "./constants/data"
 
 function App() {
   return (
-    <>
-      <h1 className="text-6xl font-bold">El Mariana</h1>
-    </>
+    <div
+      className="min-h-screen"
+      style={{ background: "#0A0A0A", fontFamily: "Barlow, sans-serif" }}
+    >
+      <Navbar />
+      <Hero />
+      <StatsBar />
+      <main>
+        {CHANNELS.map((channel, i) => (
+          <ChannelSection key={channel.id} channel={channel} index={i} />
+        ))}
+      </main>
+    </div>
   )
 }
 
